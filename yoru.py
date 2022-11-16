1a8a9f3d97feca3f6fac503f7f77df9fb7cf34cb
Sam <89331666+senawka@users.noreply.github.com>
2022-11-16 03:50:32 -0500

    Update yoru.py

--- yoru.py
+++ yoru.py
@@ -1,29 +1,81 @@
 import discord
+import random
+from discord.ext import commands
 
-client = discord.Client()
-key = ""
 
+client = commands.Bot(command_prefix='.')
 block_words = ["dylan", "catz", "minor"]
-
 
 @client.event
 async def on_ready():
     print(f"Successfully logged in as {client.user}")
 
 
-
-@client.event
-async def on_message(msg):
-    if msg.author != client.user:
-
-        for text in block_words:
-            if "Moderator" not in str(msg.author.roles) and text in str(msg.content.lower()):
-                await msg.delete()
-                return
-    if msg.author != client.user:
-        if msg.content.lower().startswith("?hi"):
-            await msg.channel.send(f"Yoru Bot is fully operational.")
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)} ms ping time")
 
 
+@client.command(aliases=["8ball", "eightball"])
+async def Eightball(ctx, *, question):
+    responses = ['Signs point to yes.',
+                 'Fuck no',
+                 'Yes.',
+                 'Yes definitely.',
+                 'Yes.',
+                 'Most likely.',
+                 'Very doubtful.',
+                 'Without a doubt.',
+                 'Most likely.',
+                 "Don't count on it.",
+                 'No',
+                 "Its not looking so good"]
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
 
-client.run(key)
+@client.command(aliases=["yon"])
+async def yesorno(ctx, *, question):
+    responses = ['Yes.',
+                 'Fuck no.',
+                 'FUCK YEAH.',
+                 'Hell no.',
+                 'No.']
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+
+
+@client.command(aliases=["Hello", "hi", "Hi"])
+async def hello(ctx):
+    await ctx.send('Hi')
+
+
+@commands.has_permissions(administrator=True)
+@client.command(aliases=['purge'])
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount + 1)
+
+
+@commands.has_permissions(kick_members=True)
+@client.command()
+async def kick(ctx, Member: discord.Member, *, reason=None):
+    await Member.kick(reason=reason)
+
+
+@commands.has_permissions(kick_members=True)
+@client.command()
+async def ban(ctx, Member: discord.Member, *, reason=None):
+    await Member.ban(reason=reason)
+
+
+@client.command(aliases=["userinfo"], help="Yummy data.")
+async def info(ctx, user: discord.Member):
+    embed = discord.Embed(title=f"{user}'s info", description=f"Here's {user}'s info", color=0x00ff00)
+    embed.add_field(name="Username:", value=user.name, inline=True)
+    embed.add_field(name="ID:", value=user.id, inline=True)
+    embed.add_field(name="Status:", value=user.status, inline=True)
+    embed.add_field(name="Highest Role:", value=user.top_role, inline=True)
+    embed.add_field(name="Joined:", value=user.joined_at, inline=True)
+    embed.set_thumbnail(url=user.avatar_url)
+    await ctx.send(embed=embed)
+
+
+
+client.run('')
